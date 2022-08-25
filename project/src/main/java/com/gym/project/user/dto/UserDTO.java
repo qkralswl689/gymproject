d00e9120f37c746190240c0452ad433102e8808e
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-08-25 19:18:07 +0900

    make UserDTO

---
+++ project/src/main/java/com/gym/project/user/dto/UserDTO.java
@@ -0,0 +1,59 @@
+package com.gym.project.user.dto;
+
+import com.gym.project.board.entity.BoardEntity;
+import com.gym.project.user.entity.UserEntity;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UserDTO {
+
+    private int userIndex;
+    private String userEmail;
+    private String userPw;
+    private String userName;
+    private String gymName;
+    private boolean socialYN;
+    private String userClass;
+    private String phoneNumber;
+    private Date userBirth;
+
+    public UserEntity toEntity(){
+        UserEntity build = UserEntity.builder()
+                .userIndex(userIndex)
+                .userEmail(userEmail)
+                .userPw(userPw)
+                .userName(userName)
+                .gymName(gymName)
+                .socialYN(socialYN)
+                .userClass(userClass)
+                .phoneNumber(phoneNumber)
+                .userBirth(userBirth)
+                .build();
+        return build;
+
+    }
+
+    @Builder
+    public UserDTO( int userIndex,String userEmail,String userPw,String userName,String gymName,boolean socialYN,String userClass,String phoneNumber,Date userBirth){
+        this.userIndex = userIndex;
+        this.userEmail = userEmail;
+        this.userPw = userPw;
+        this.userName = userName;
+        this.gymName = gymName;
+        this.socialYN = socialYN;
+        this.userClass = userClass;
+        this.phoneNumber = phoneNumber;
+        this.userBirth = userBirth;
+
+    }
+}
--- project/src/main/java/com/gym/project/user/entity/UserEntity.java
+++ project/src/main/java/com/gym/project/user/entity/UserEntity.java
@@ -16,23 +16,32 @@ import java.util.Set;
 public class UserEntity extends BaseEntity{
 
     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "userindex")
     private int userIndex;
 
     @Id
+    @Column(name = "useremail")
     private String userEmail;
 
+    @Column(name = "userpw")
     private String userPw;
 
+    @Column(name = "username")
     private String userName;
 
+    @Column(name = "gymname")
     private String gymName;
 
-    private String socialYN;
+    @Column(name = "socialyn")
+    private boolean socialYN;
 
+    @Column(name = "userclass")
     private String userClass;
 
+    @Column(name = "phonenumber")
     private String phoneNumber;
 
+    @Column(name = "userbirth")
     private Date userBirth;
 
 
