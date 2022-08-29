a44ace1e7422524fcd77d965fe0dce2c900c453c
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-08-29 19:26:01 +0900

    import login and join ui

--- project/src/main/java/com/gym/project/controller/GymProjectController.java
+++ project/src/main/java/com/gym/project/controller/GymProjectController.java
@@ -5,11 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/")
 public class GymProjectController {
 
+
     public String Hello(Model model){
         return "index.html";
+    }
+
+
+    @GetMapping("/login")
+    public String Login(HttpSession session, Model model){
+        return "html/login";
+    }
+
+    @GetMapping("/join")
+    public String Join(HttpSession session, Model model){
+        return "html/join";
     }
 }
